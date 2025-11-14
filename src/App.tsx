26575893e3a8504d86ce677a869d6256470042c2
guytz1 <guybentzur2@gmail.com>
2025-11-14 22:44:56 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,20 @@
+import { ScrollPopup } from "@/components/ScrollPopup";
+
+function App() {
+  return (
+    <>
+      <ScrollPopup />
+
+      {/* Your site sections */}
+      <Hero />
+      <Features />
+      <Contact />
+    </>
+  );
+}
+
+
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
